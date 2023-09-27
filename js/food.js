43eeb97f12c3585b120537da0c2bf9479b4f4bf4
Daniel Egele <danielegele9@gmail.com>
2023-09-27 20:28:46 +0100

    all added files

---
+++ js/food.js
@@ -0,0 +1,34 @@
+// Q1
+let favoriteFoods = ["Ripped plantain", "Drawsoup", "white rice/peppersoup"];
+// // Q2
+// console.log(favoriteFoods[2]);
+// // Q3
+// favoriteFoods.splice(3,0,"Moimoi");
+// console.log(favoriteFoods);
+// // Q4
+//  favoriteFoods.shift();
+//  let Rippedplantain = [...favoriteFoods];
+//  console.log(Rippedplantain);
+// // Q5
+// favoriteFoods.push("Brownbeans");
+// console.log(favoriteFoods);
+// // Q6
+//    favoriteFoods.unshift("Eggsauce");
+//    console.log(favoriteFoods);
+
+// Q7
+
+// // Q8a
+// let num = [2, 3, 4, 5];
+// let removeElement = num.splice(1,1);
+// console.log(num);
+
+// // Q8b
+// let nuclear = ["alpha", "gamma", "delta"];
+// nuclear.splice(1,0, "beta");
+// console.log(nuclear);
+// Q8c
+
+
+
+
